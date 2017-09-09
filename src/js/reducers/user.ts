a5817ea0aad18e0859a248f974f01ef9c7503bfe
philippe.roy <philippe.roy@carsales.com.au>
2017-09-09 11:49:19 +1000

    Refactor reducer to use object spread syntax

--- src/js/reducers/user.ts
+++ src/js/reducers/user.ts
@@ -5,28 +5,31 @@ const initialState = {
     fetched: false,
     error: null
 }
-  
+ 
+// We use the the object spread syntax (...) as it is less verbose that using 
+// Object.assign. We are also making sure we use a return in each case to break out 
+// of the switch statement
 function user(state = initialState, action) {
     switch (action.type) {
         case 'SET_NAME':
-            state = {
+            return {
                 ...state,
                 name: action.payload
             }
         case 'FETCH_USER':
-            state = {
+            return {
                 ...state,
                 fetching: true
             }
         case 'FETCH_USER_FULLFILLED':
-            state = {
+            return {
                 ...state,
                 fetching: false,
                 fetched: true,
                 user: action.payload,
             }
         case 'FETCH_USER_REJECTED':
-            state = {
+            return {
                 ...state,
                 fetching: false,
                 error: action.payload
