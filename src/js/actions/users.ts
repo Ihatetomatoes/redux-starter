6defc0f05ae5b70cc6305d71b5563c94af9d334c
philippe.roy <philippe.roy@carsales.com.au>
2017-09-09 12:16:09 +1000

    Add thunk

--- src/js/actions/users.ts
+++ src/js/actions/users.ts
@@ -3,21 +3,43 @@ export const setName = (name:string) => ({
     payload: name
 })
 
-export const fetchUser = () => {
+
+// Thunk Action 
+// Thunk middleware knows how to handle functions.
+// It passes the dispatch method as an argument to the function,
+// thus making it able to dispatch actions itself.
+export const fetchUser = (id: number) => {
+    // First dispatch: the app state is updated to inform
+    // that the API call is starting.
     return (dispatch) => {
-        fetch('https://jsonplaceholder.typicode.com/users/1')
-            .then(response => response.json())
-            .then(parsedJSON => {
-                dispatch({
-                    type: "FETCH_USER",
-                    payload: parsedJSON
-                })
-            })
-            .catch((error) => {
-                dispatch({
-                    type: "FETCH_USER_REJECTED",
-                    payload: error
-                })
-            })
+        
+        // First dispatch: the app state is updated to inform
+        // that the API call is starting.
+        dispatch({type: "FETCH_USER"})
+
+        // We use fetch to make our API call
+        // Fetch is promised based and will dispatch an aysncronous
+        // action when resolved or rejected
+        fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+            .then(
+                response => response.json()         
+            )
+            .then(
+                // If successful
+                parsedJSON => {
+                    dispatch({
+                        type: "FETCH_USER_FULLFILLED",
+                        payload: parsedJSON
+                    })
+                },
+                // If there was an error
+                error => (         
+                     dispatch({
+                        type: "FETCH_USER_REJECTED",
+                        payload: error
+                    })
+                )  
+            )
+
     }
 }
